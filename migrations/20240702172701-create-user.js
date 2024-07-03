179923485d2cdf6e31131aea188d7a69271d4a6c
goldenfinix68 <goldenfinix@outlook.com>
2024-07-03 17:03:09 -0400

    Refactor user & category migration

--- migrations/20240702172701-create-user.js
+++ migrations/20240702172701-create-user.js
@@ -1,47 +1,40 @@
 "use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable(
-      "users",
-      {
-        id: {
-          allowNull: false,
-          primaryKey: true,
-          type: Sequelize.STRING,
-        },
-        fullname: {
-          allowNull: false,
-          type: Sequelize.STRING,
-        },
-        email: {
-          type: Sequelize.STRING,
-          allowNull: false,
-          unique: true,
-          validate: {
-            isEmail: true,
-          },
-        },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-        },
-        updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-          defaultValue: Sequelize.literal(
-            "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"
-          ),
-        },
-        deletedAt: {
-          type: Sequelize.DATE,
+    await queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.STRING,
+      },
+      fullname: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
         },
       },
-      {
-        timestamps: true,
-        paranoid: true,
-      }
-    );
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal(
+          "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"
+        ),
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+      },
+    });
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable("users");
