ca9c6c6ef8100b9a46aa7899ee70fca0b8fb2a59
goldenfinix68 <goldenfinix@outlook.com>
2024-07-04 03:37:49 -0400

    Add validate middleware in faq route

--- routes/faqRoute.js
+++ routes/faqRoute.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const faqController = require("../controller/faqController");
+const { validate } = require("../middleware");
+const { faqValidation } = require("../validation");
 
-router.get("/fetchFaqsByCategory", faqController.getFaqsByCategory);
+router.get(
+  "/fetchFaqsByCategory",
+  validate(faqValidation.getFaqsByCategory),
+  faqController.getFaqsByCategory
+);
 
 router.get("/fetchFaqById", faqController.getFaqById);
 
