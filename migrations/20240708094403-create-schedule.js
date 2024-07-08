2b73f9e13222bf271455e844b65c635fa2933619
goldenfinix68 <goldenfinix@outlook.com>
2024-07-08 06:23:23 -0400

    Add schedule model & migration

---
+++ migrations/20240708094403-create-schedule.js
@@ -0,0 +1,60 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("schedules", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      from: {
+        type: Sequelize.INTEGER,
+      },
+      to: {
+        type: Sequelize.INTEGER,
+      },
+      content: {
+        type: Sequelize.STRING,
+      },
+      type: {
+        type: Sequelize.STRING,
+      },
+      day: {
+        type: Sequelize.INTEGER,
+      },
+      image: {
+        type: Sequelize.STRING,
+        defaultValue: "",
+      },
+      tourId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "tours",
+          key: "id",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal(
+          "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"
+        ),
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("schedules");
+  },
+};
---
+++ models/schedule.js
@@ -0,0 +1,43 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class schedule extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      schedule.belongsTo(models.tour, { foreignKey: "tourId", as: "tour" });
+    }
+  }
+  schedule.init(
+    {
+      title: DataTypes.STRING,
+      from: DataTypes.INTEGER,
+      to: DataTypes.INTEGER,
+      content: DataTypes.STRING,
+      type: DataTypes.STRING,
+      day: DataTypes.INTEGER,
+      image: {
+        type: DataTypes.STRING,
+        defaultValue: "",
+      },
+      tourId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "tour",
+          key: "id",
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "schedule",
+      paranoid: true,
+      timestamps: true,
+    }
+  );
+  return schedule;
+};
