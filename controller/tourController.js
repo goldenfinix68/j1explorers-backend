0db26067cdf59418dddcdbb4acc06a2ea77913e4
goldenfinix68 <goldenfinix@outlook.com>
2024-07-08 15:32:08 -0400

    Fix scheduleDetail import

--- controller/tourController.js
+++ controller/tourController.js
@@ -4,6 +4,7 @@ const ApiError = require("../utils/ApiError");
 const httpStatus = require("http-status");
 const db = require("../models");
 const { tourAttributes } = require("../consts");
+const { scheduleDetail } = require("../consts/scheduleAttributes");
 
 const getAll = catchSync(async (req, res) => {
   const tours = await tourService.getAllTours();
