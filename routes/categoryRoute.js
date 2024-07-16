90c10266539b0c0f3f7bf5ffe2d2da9214a67644
goldenfinix68 <goldenfinix@outlook.com>
2024-07-16 04:28:08 -0400

    Add authenticate middleware to category route

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const categoryController = require("../controller/categoryController");
-const { validate } = require("../middleware");
+const { validate, authenticate } = require("../middleware");
 const { categoryValidation } = require("../validation");
 
 router.get("/fetchCategories", categoryController.getAll);
 
 router.get(
   "/fetchCategory",
+  authenticate,
   validate(categoryValidation.getCategory),
   categoryController.getCategoryById
 );
