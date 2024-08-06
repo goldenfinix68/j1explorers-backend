d9f18ac56210ba504b884d0f4c118c0b17da4303
goldenfinix68 <goldenfinix@outlook.com>
2024-08-06 02:48:24 -0400

    Update categoryService with categoryAttributes

--- service/categoryService.js
+++ service/categoryService.js
@@ -1,14 +1,15 @@
 const db = require("../models");
+const { categoryAttributes } = require("../consts");
 
 const getAllCategories = () =>
   db.category.findAll({
-    attributes: ["id", "title"],
+    attributes: [...categoryAttributes.categoryDetail],
     where: { deletedAt: null },
   });
 
 const getCategoryById = (id) =>
   db.category.findOne({
-    attributes: ["id", "title"],
+    attributes: [...categoryAttributes.categoryDetail],
     where: { deletedAt: null, id: Number(id) },
   });
 
